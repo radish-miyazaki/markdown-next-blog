1605ee2e9743b29335e869c348435477c96130eb
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2021-07-14 14:46:38 +0900

    create category list component

--- pages/blog/category/[category_name].tsx
+++ pages/blog/category/[category_name].tsx
@@ -1,20 +1,29 @@
-import Layout from "@/components/Layout";
+import React from "react";
 import * as fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import Layout from "@/components/Layout";
 import Post from "@/components/Post";
 import getPosts from "@/lib/posts";
+import CategoryList from "@/components/CategoryList";
 
-const CategoryBlogPage = ({posts, categoryName}) => {
+const CategoryBlogPage = ({posts, categoryName, categories}) => {
   return (
     <Layout>
-      <h1 className="text-4xl border-b-4 p-5 font-bold">Posts in {categoryName}</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {
-          posts.map((post, index) => (
-            <Post key={index} post={post} />
-          ))
-        }
+      <div className="flex justify-between">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-4xl border-b-4 p-5 font-bold">Posts in {categoryName}</h1>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {
+              posts.map((post, index) => (
+                <Post key={index} post={post} />
+              ))
+            }
+          </div>
+        </div>
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   )
@@ -44,17 +53,23 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = ({params}) => {
   const {category_name} = params
+  const posts = getPosts()
 
   // Filter posts by category
-  const categoryPosts = getPosts().filter(post => post.frontmatter.category.toLowerCase() === category_name)
+  const categoryPosts = posts.filter(post => post.frontmatter.category.toLowerCase() === category_name)
 
   // get to display category name
   const rawCategoryName = categoryPosts[0].frontmatter.category
+
+  // Get categories fot sidebar
+  const categories = posts.map(post => post.frontmatter.category)
+  const uniqueCategories = [...Array.from(new Set(categories))]
 
   return {
     props: {
       posts: categoryPosts,
       categoryName: rawCategoryName,
+      categories: uniqueCategories,
     }
   }
 }
--- pages/blog/page/[page_index].tsx
+++ pages/blog/page/[page_index].tsx
@@ -6,19 +6,28 @@ import Post from "@/components/Post";
 import Pagination from "@/components/Pagination";
 import {POSTS_PER_PAGE} from "@/config/index";
 import getPosts from "@/lib/posts";
+import CategoryList from "@/components/CategoryList";
 
-const BlogPage = ({posts, numPages, currentPage}) => {
+const BlogPage = ({posts, numPages, currentPage, categories}) => {
   return (
     <Layout>
-      <h1 className="text-4xl border-b-4 p-5 font-bold">Blog</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {
-          posts.map((post, index) => (
-            <Post key={index} post={post} />
-          ))
-        }
+
+      <div className="flex justify-between">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-4xl border-b-4 p-5 font-bold">Blog</h1>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {
+              posts.map((post, index) => (
+                <Post key={index} post={post} />
+              ))
+            }
+          </div>
+          <Pagination currentPage={currentPage} numPages={numPages} />
+        </div>
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
-      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   )
 }
@@ -47,19 +56,24 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = ({params}) => {
   const page = parseInt((params && params.page_index || 1))
-
   const files = fs.readdirSync(path.join('posts'))
+  const posts = getPosts()
+
+  // Get categories fot sidebar
+  const categories = posts.map(post => post.frontmatter.category)
+  const uniqueCategories = [...Array.from(new Set(categories))]
 
   // Pagination
   const numPages = Math.ceil(files.length /POSTS_PER_PAGE)
   const pageIndex = page - 1
-  const orderedPosts = getPosts().slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+  const orderedPosts = posts.slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
 
   return {
     props: {
       posts: orderedPosts,
       numPages,
       currentPage: page,
+      categories: uniqueCategories,
     }
   }
 }
